a34100eddefb208292a34e4c5353482f9a3cb4a9
reinaldocantidio <reinaldocantidio@gmail.com>
2023-03-07 20:10:41 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /* String nomeCompleto = "Linguagem " + "JAVA";
+        System.out.println(nomeCompleto); */
+
+        int a, b;
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+        int resultado2 = a==b ? 1 : 0;
+
+        
+
+        System.out.println(resultado);
+        System.out.println(resultado2);
+
+    }
+}
