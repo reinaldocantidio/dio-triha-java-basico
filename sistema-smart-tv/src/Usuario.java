d994a7ecf787da54f6da955f02e898d42b5b0462
reinaldocantidio <reinaldocantidio@gmail.com>
2023-03-07 21:15:36 -0300

    Metodos - Ex. SmartTv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,30 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual " + smartTv.volume);
+
+
+        
+
+        smartTv.mudarDeCanal(13);
+        System.out.println("Canal atual " + smartTv.canal);
+
+        System.out.println("TV ligada ?" + smartTv.ligada);
+        System.out.println("Canal atual " + smartTv.canal);
+        System.out.println("Volume atual " + smartTv.volume);
+
+
+        smartTv.ligar();
+        System.out.println("Novo status - TV ligada ?" + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status - TV ligada ?" + smartTv.ligada);
+
+    }
+}
